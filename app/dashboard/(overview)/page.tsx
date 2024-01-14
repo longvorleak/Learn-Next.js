695117559fbed3fdecc1586780834efa760d6586
Vorleak <longvorleak@gmail.com>
2024-01-14 21:35:05 +0900

    chapter 9

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,7 +10,6 @@ import {
  } from '@/app/ui/skeletons';
  
 export default async function Page() {
-  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
