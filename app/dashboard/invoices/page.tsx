a72dea70c81503fb35dfe888340cbd713f038665
Vorleak <longvorleak@gmail.com>
2024-01-28 19:52:28 +0900

    fix pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,15 +3,11 @@ import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
-import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { fetchInvoicesPages } from '@/app/lib/data';
-import { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Invoices',
-};
-
+ 
 export default async function Page({
   searchParams,
 }: {
@@ -24,7 +20,7 @@ export default async function Page({
   const currentPage = Number(searchParams?.page) || 1;
 
   const totalPages = await fetchInvoicesPages(query);
-
+ 
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
